fb763ea7da26226181dc81ddd01a5ef45947c327
Łukasz Wójcik <lwojcik@lukem.net>
2018-11-29 22:43:15 +0100

    removing some folders

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var utils_1 = require("./lib/utils");
-exports.BattleNetUtils = utils_1.BattleNetUtils;
-//# sourceMappingURL=index.js.map
