c73103dde23b5035faa46f2883b88415e6128653
Łukasz Wójcik <lwojcik@lukem.net>
2018-12-14 20:32:18 +0100

    unfinished sc2realms

--- src/lib/utils/localization/sc2realms.ts
+++ src/lib/utils/localization/sc2realms.ts
@@ -1,6 +1,6 @@
 import constants from '../../constants';
-
-// import { RegionIdAsNumberOrString, RegionIdArray, RegionName, RegionNameArray } from '../../types';
+import { validateRegionId } from './regions';
+import { RegionIdAsNumberOrString, Locale } from '../../types';
 
 /**
  * Returns a list of all available StarCraft II realms
@@ -11,84 +11,75 @@ export function getAllSc2Realms() {
   return constants.SC2_REALMS;
 }
 
-// /**
-//  * Returns a list of all available region ids
-//  *
-//  * @return List of all available regions as flat array of numbers.
-//  */
-// export function getAllRegionIds(): RegionIdArray {
-//   const regionKeys = Object.keys(constants.REGIONS);
-//   const regionKeysAsIntegers = regionKeys.map(regionKey => parseInt(regionKey, 10));
-//   return regionKeysAsIntegers;
-// }
+/**
+ * Returns StarCraft II realms for given region id
+ *
+ * @param regionId Region id as integer or string
+ * @return List of SC2 realms available in a specific region as array of integers
+ */
+export function getSc2RealmsByRegionId(regionId: RegionIdAsNumberOrString) {
+  const regionIdAsString = regionId.toString();
+  const isRegionIdValid = validateRegionId(regionIdAsString);
 
-// /**
-//  * Returns a list of all available region names
-//  *
-//  * @return {Array} List of all available regions as flat array of strings.
-//  */
-// export function getAllRegionNames(): RegionNameArray {
-//   const regionNames = Object.values(constants.REGIONS);
-//   const flattenedRegionNames = ([] as (RegionName | RegionNameArray)[]).concat(...regionNames);
-//   const regionNamesAsStrings = flattenedRegionNames.map((regionName: any) => regionName.toString());
-//   return regionNamesAsStrings;
-// }
+  if (!isRegionIdValid) {
+    throw new RangeError(`${regionIdAsString} is not a valid parameter for getLocalesByRegionId()`);
+  }
 
-// /**
-//  * Returns region name(s) represented by given region id
-//  *
-//  * @param regionId Region id as integer or string
-//  * @return Region name represented as two-letter code (e.g. "us" for Americas) or an array of regions
-//  * if more than one is specified for a given region id
-//  */
-// export function getRegionNameById(regionId: RegionIdAsNumberOrString) {
-//   const regionIds = Object.keys(constants.REGIONS);
-//   const regionIdAsString = regionId.toString();
-//   const isRegionIdValid = regionIds.includes(regionIdAsString);
+  return constants.SC2_REALMS[regionIdAsString];
+}
 
-//   if (!isRegionIdValid) {
-//     throw new RangeError(`${regionIdAsString} is not a valid parameter for getRegionNameById()`);
-//   }
+/**
+ * Verifies whether locale matches the regex pattern
+ *
+ * @param locale Locale name
+ * @return true if locale matches the pattern, false if not
+ */
+export function checkIfSc2RealmLooksValid(locale: Locale) {
+  const localeRegexPattern = /^(?:[a-z]{2}_[a-z]{2})$/gi;
+  const doesLocaleLookValid = localeRegexPattern.test(locale);
+  localeRegexPattern.lastIndex = 0;
+  return doesLocaleLookValid;
+}
 
-//   return constants.REGIONS[regionIdAsString];
-// }
+/**
+ * Validates locale name against locale list (whether it exists in the constants object)
+ *
+ * @param locale Locale name
+ * @return true if locale exists, false if not. Throws RangeError if locale doesn't match regex pattern
+ */
+export function validateSc2Realm(locale: Locale) {
+  const doesLocaleLookValid = checkIfLocaleLooksValid(locale);
 
-// /**
-//  * Validates region id provided as number or string
-//  *
-//  * @param regionId Region id as integer or string
-//  * @return true for valid region id. false for invalid region id
-//  */
-// export function validateRegionId(regionId: RegionIdAsNumberOrString) {
-//   return Boolean(getRegionNameById(regionId));
-// }
+  if (!doesLocaleLookValid) {
+    throw new RangeError(`${locale} is not a valid parameter for validateLocale()`);
+  }
 
-// /**
-//  * Returns region id for given region name
-//  *
-//  * @param regionName Region id as integer or string
-//  * @return Region id as number
-//  */
-// export function getRegionIdByName(regionName: RegionName) {
-//   const regionNameLowercase = regionName.toLowerCase();
-//   const regions = constants.REGIONS;
-//   const regionKeys = Object.keys(regions);
-//   const regionIdArray = regionKeys.filter(key => regions[key].includes(regionNameLowercase));
-//   const regionId = Number(regionIdArray[0]) || false;
+  const lowerCaseLocaleList = getAllLocaleNames().map(localeName => localeName.toLowerCase());
+  const lowerCaseLocale = locale.toLowerCase();
+  return lowerCaseLocaleList.includes(lowerCaseLocale);
+}
 
-//   if (!regionId) {
-//     throw new RangeError(`"${regionName}" is not a valid parameter for getRegionIdByName()`);
-//   }
+/**
+ * Checks whether given locale is available for a given region id
+ *
+ * @param locale Locale name
+ * @param regionId Region id as integer or string
+ * @return List of locales available in a specific region as array of strings
+ */
+export function isSc2RealmValidForRegionId(locale: Locale, regionId: RegionIdAsNumberOrString) {
+  const lowerCaseLocale = locale.toLowerCase();
+  const doesLocaleLookValid = validateSc2Realm(lowerCaseLocale) || false;
+  const regionIdAsString = regionId.toString();
+  const isRegionIdValid = validateRegionId(regionIdAsString);
 
-//   return regionId;
-// }
+  if (!doesLocaleLookValid) {
+    throw new RangeError(`${locale} is not a valid locale parameter for isLocaleValidForRegionId()`);
+  }
 
-// /**
-//  * Validates region name provided as string
-//  *
-//  * @param regionId Region name
-//  * @return true for valid region name. false for invalid region name
-//  */
-// export function validateRegionName(regionName: RegionName) {
-//   return Boolean(getRegionIdByName(regionName));
-// }
+  if (!isRegionIdValid) {
+    throw new RangeError(`${regionId} is not a valid regionId parameter for isLocaleValidForRegionId()`);
+  }
+
+  const localesForRegionId = getSc2RealmsByRegionId(regionId).map(sc2realm => localeName.toLowerCase());
+  return localesForRegionId.includes(lowerCaseLocale);
+}
