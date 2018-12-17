b143156d29734fe237f6679e5d8d4351b53171aa
Łukasz Wójcik <lwojcik@lukem.net>
2018-12-17 20:16:48 +0100

    helper functions: validateUri + fetchFromUri

---
+++ src/lib/helpers/index.ts
@@ -0,0 +1,37 @@
+import { default as fetch } from 'cross-fetch';
+import { Uri } from '../types';
+
+/**
+ * Validates uri against regex pattern
+ * @function
+ * @param {string} uri Fetch request uri (e.g. https://example.com/api/endpoint)
+ * @returns {Boolean} True for valid uri, false for incorrect one
+ */
+export function validateUri(uri:Uri) {
+  // via https://www.regextester.com/94502
+  const uriTestRegex = /^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
+  uriTestRegex.lastIndex = 0;
+  return uriTestRegex.test(uri);
+}
+
+/**
+ * Performs basic fetch request from given uri
+ * @function
+ * @param {string} uri Fetch request uri
+ * @returns {object} Data returned by requested uri
+ */
+export async function fetchFromUri(uri:Uri) {
+  const isUriValid = validateUri(uri);
+
+  if (!isUriValid) {
+    throw new Error(`'${uri}' is not a valid parameter for fetchFromUri()`);
+  }
+
+  try {
+    const data = await fetch(uri);
+    const response = await data.json();
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
