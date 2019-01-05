82aaf428b5832b921edf707c8b3e30f7e5ac96b4
Łukasz Wójcik <lwojcik@lukem.net>
2019-01-05 21:53:08 +0100

    add endpoint query mechanics

---
+++ src/lib/helpers/bnet.ts
@@ -0,0 +1,20 @@
+import { RegionIdOrName, AccessToken } from '../types';
+
+// import { fetchFromUri } from './fetch';
+
+// construct checkTokenUri
+// construct getTokenUri
+
+export function queryEndpoint(region: RegionIdOrName, endpoint:string, accessToken:AccessToken) {
+
+  console.log('querying endpoint');
+  console.log(region);
+  console.log(endpoint);
+  console.log(accessToken);
+  // TODO
+  return '123456789';
+}
+
+// getAccessToken
+// validateAccessToken
+// checkAccessToken
