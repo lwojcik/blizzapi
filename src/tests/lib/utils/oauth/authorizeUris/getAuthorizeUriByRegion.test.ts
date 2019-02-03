e3acaf7fe665dc2579674b79cc83a845146788c6
Łukasz Wójcik <lwojcik@lukem.net>
2019-02-03 01:13:46 +0100

    fix tslint erros

--- src/tests/lib/utils/oauth/authorizeUris/getAuthorizeUriByRegion.test.ts
+++ src/tests/lib/utils/oauth/authorizeUris/getAuthorizeUriByRegion.test.ts
@@ -6,6 +6,7 @@ import wrongRegionNamesJson from '../../../../__testData__/wrongRegionNames.json
 import regionIdsJson from '../../../../__testData__/regionIds.json';
 import wrongRegionIdsJson from '../../../../__testData__/wrongRegionIds.json';
 
+/* tslint:disable no-expression-statement */
 describe('getAuthorizeUriByRegion()', () => {
   test('should be defined', () => {
     expect(getAuthorizeUriByRegion).toBeDefined();
@@ -15,27 +16,28 @@ describe('getAuthorizeUriByRegion()', () => {
     expect(typeof getAuthorizeUriByRegion).toBe('function');
   });
 
-  (regionIdsJson as (string | number)[]).forEach(regionId => {
+  (regionIdsJson as ReadonlyArray<number | string>).forEach(regionId => {
     test(`should return correct value for ${regionId} as valid region id`, () => {
       expect(getAuthorizeUriByRegion(regionId)).toMatchSnapshot();
     });
   });
 
-  (wrongRegionIdsJson as (string | number)[]).forEach(wrongRegionId => {
+  (wrongRegionIdsJson as ReadonlyArray<number | string>).forEach(wrongRegionId => {
     test(`should throw RangeError for ${wrongRegionId} as invalid region id`, () => {
       expect(() => getAuthorizeUriByRegion(wrongRegionId)).toThrow(RangeError);
     });
   });
 
-  (regionNamesJson as string[]).forEach(regionName => {
+  (regionNamesJson as ReadonlyArray<string>).forEach(regionName => {
     test(`should return correct value for ${regionName} as valid region name`, () => {
       expect(getAuthorizeUriByRegion(regionName)).toMatchSnapshot();
     });
   });
 
-  (wrongRegionNamesJson as string[]).forEach(wrongRegionName => {
+  (wrongRegionNamesJson as ReadonlyArray<string>).forEach(wrongRegionName => {
     test(`should throw RangeError for invalid region name`, () => {
       expect(() => getAuthorizeUriByRegion(wrongRegionName)).toThrow(RangeError);
     });
   });
 });
+/* tslint:enable no-expression-statement */
