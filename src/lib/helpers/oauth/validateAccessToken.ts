1fab034a89e3fa802f60101356dc6ad6bd3dc5d3
Łukasz Wójcik <lwojcik@lukem.net>
2019-02-12 00:12:40 +0100

    remove console.log

--- src/lib/helpers/oauth/validateAccessToken.ts
+++ src/lib/helpers/oauth/validateAccessToken.ts
@@ -4,7 +4,6 @@ import { fetchFromUri } from '../../helpers/fetch';
 
 export default async (regionIdOrName: RegionIdOrName, accessToken: AccessToken) => {
   const checkTokenUri = getCheckTokenUriByRegion(regionIdOrName);
-  console.log(checkTokenUri);
   const requestPath = `${checkTokenUri}${accessToken}`;
   const response = await fetchFromUri(requestPath);
   return response;
