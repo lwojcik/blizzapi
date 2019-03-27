3f32faf6777827cee449ec443aec3cc54f7bd167
Łukasz Wójcik <lwojcik@lukem.net>
2019-03-27 21:40:38 +0100

    coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -25,13 +25,13 @@ module.exports = {
   "automock": false,
   "setupFiles": [
     "./setupJest.ts"
-  ]
-  // "coverageThreshold": {
-  //   "global": {
-  //     "branches": 100,
-  //     "functions": 100,
-  //     "lines": 100,
-  //     "statements": 100
-  //   }
-  // }
+  ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 100,
+      "functions": 100,
+      "lines": 100,
+      "statements": 100
+    }
+  }
 }
