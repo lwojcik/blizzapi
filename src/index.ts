91ce762e5334eee0088f8f21c54ac6e0967ec752
Łukasz Wójcik <lwojcik@lukem.net>
2019-08-01 17:59:25 +0200

    export ts types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 import BlizzAPI from './lib/classes/BlizzAPI';
-export = BlizzAPI;
+
+export * from '../@types';
+export * from '../@interfaces';
+export default BlizzAPI;
+
+/* istanbul ignore next */
+// tslint:disable no-object-mutation
+if (module) module.exports = BlizzAPI;
