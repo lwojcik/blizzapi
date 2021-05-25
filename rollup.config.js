5185c10bed86641163db64647b8149104a452863
Łukasz Wójcik <lwojcik@lukem.net>
2021-05-25 19:14:53 +0200

    sync ESLint rules with StarCraft2-API

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,10 @@ import externalGlobals from 'rollup-plugin-external-globals';
 
 const name = require('./package.json').main.replace(/\.js$/, '');
 
-const bundle = (config) => ({
+const bundle = config => ({
   ...config,
   input: 'src/index.ts',
-  external: (id) => !/^[./]/.test(id),
+  external: id => !/^[./]/.test(id),
 });
 
 export default [
